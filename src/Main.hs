1e83239d58005c629ea78fe8fff98f173528f32f
morten <morten@aeai.dk>
2015-05-02 17:26:16 +0200

    Started Tinglysning.dk work.

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 import Text.HTML.TagSoup (parseTags, Tag, Tag(..), (~==), (~/=), sections, fromTagText, isTagText)
 import Network.HTTP (getResponseBody, getRequest, simpleHTTP, urlEncode)
+import Network.HTTP.Conduit
 import Control.Exception
 import Data.List
 import Data.Char
+import qualified Data.ByteString.Lazy.Char8 as L
 
 -- Get VIN from trafikstyrelsen.
 getVIN :: String -> IO String
@@ -20,7 +23,7 @@ getVIN' :: String -> String
 getVIN' a = first $ filter isValid (getVINCandidates a)
 
 first :: [String] -> String
-first [] = "No VIM found by parser."
+first [] = "No VIM found. Or the parser failed."
 first a = head a
 
 -- Validator for VIN.
@@ -44,7 +47,29 @@ getHTML url = do
   resp <- simpleHTTP $ getRequest $ url
   html <- getResponseBody resp
   return html
-    
 
 
+
+--soegemaade:content:center:bilbogen:stelnrOption
+--content:center:bilbogen:stelnr:WAUZZZ8P2AA090943
+--content:center:bilbogen:cvr:
+--content:center:bilbogen:navn:
+--content:center:bilbogen:foedselsdato:
+--bogsattest:content:center:bilbogen:uofficiel
+--org.apache.myfaces.trinidad.faces.FORM:j_id4
+--_noJavaScript:false
+--javax.faces.ViewState:!-pwa1crud3
+--source:content:center:bilbogen:j_id150
+
+getHTMLTLS = do
+  req0 <- parseUrl "https://www.tinglysning.dk/tinglysning/forespoerg/bilbogen/bilbogen.xhtml"
+  let req = req0 {
+        --method = methodPost
+        requestHeaders = [("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:37.0) Gecko/20100101 Firefox/37.0")]
+                                  --, requestBody = RequestBodyLBS "{\"longUrl\": \"http://www.google.com/\"}"
+        }
+
+  res <- withManager $ httpLbs req
+  L.putStrLn $ responseBody res
+  
 
