863393c9c6c0738ce1f3f9ac24be4c5660210711
morten <morten@aeai.dk>
2015-05-02 13:29:45 +0200

    Refactoring.

--- src/Main.hs
+++ src/Main.hs
@@ -5,24 +5,6 @@ import Control.Exception
 import Data.List
 import Data.Char
 
--- Refine candidates from the HTML soup. Yeah, it's a convoluted process...
-getVINCandidates :: String -> [String]
-getVINCandidates a = dequote $ map f $ filter isTagText (parseTags a)
-  where f = unwords . words . fromTagText
-        dequote = filter (not . null)
-
--- We're assuming that we're looking for a VIN, so filter the candidates for one.
--- This approach will fail if the tagsoup contains more than one VIN.
-getVIN' :: String -> String
-getVIN' a = head $ filter isValid (getVINCandidates a)
-            where isValid a = length a == 17 && (and $ map isDigitOrUpperLetter a)
-
-isDigitOrUpperLetter :: Char -> Bool
-isDigitOrUpperLetter a
-  | isDigit a = True 
-  | isLetter a && isUpper a = True
-  | otherwise = False
-
 -- Get VIN from trafikstyrelsen.
 getVIN :: String -> IO String
 getVIN a = do
@@ -31,6 +13,31 @@ getVIN a = do
   case result of
    Left ex -> return $ show ex
    Right html -> return $ getVIN' html
+
+-- We're assuming that we're looking for a VIN, so filter the candidates for one.
+-- This approach will fail if the tagsoup contains more than one VIN.
+getVIN' :: String -> String
+getVIN' a = first $ filter isValid (getVINCandidates a)
+
+first :: [String] -> String
+first [] = "No VIM found by parser."
+first a = head a
+
+-- Validator for VIN.
+isValid :: String -> Bool
+isValid a = length a == 17 && (and $ map isDigitOrUpperLetter a)
+
+isDigitOrUpperLetter :: Char -> Bool
+isDigitOrUpperLetter a
+  | isDigit a = True 
+  | isLetter a && isUpper a = True
+  | otherwise = False
+
+-- Refine candidates from the HTML soup. Yeah, it's a convoluted process...
+getVINCandidates :: String -> [String]
+getVINCandidates a = dequote $ map f $ filter isTagText (parseTags a)
+  where f = unwords . words . fromTagText
+        dequote = filter (not . null)
 
 getHTML :: String -> IO String
 getHTML url = do
