16cf5ddf474f8f6c1483ad9a0c3ee31576bd27c9
morten <morten@aeai.dk>
2015-05-05 22:13:45 +0200

    Removing trial html.

--- src/Main.hs
+++ src/Main.hs
@@ -9,8 +9,6 @@ import Data.Char
 import qualified Data.ByteString.Lazy.Char8 as L
 
 
-t = "<div class=\"horizontalSplitter\"></div>\r\n\r\n        <div class=\"floatLeft grid5\">\r\n            <h2>K\195\184ret\195\184j</h2>\r\n            \r\n            <div id=\"ctl00_m_g_4156985a_4cd3_409b_aab5_4416025b40bb_ctl00_pnlVehicleInfo\">\r\n\t\t\t\t\t\t\r\n            <div class=\"pairName\">M\195\166rke</div>\r\n            <div class=\"pairValue\">AUDI</div>\r\n            <div class=\"pairName\">Model</div>\r\n            <div class=\"pairValue\">A3</div>\r\n            <div class=\"pairName\">Stelnummer</div>\r\n            <div class=\"pairValue\">WAUZZZ8P2AA090943</div>\r\n            <div class=\"pairName\">Seneste reg.nr.</div>\r\n            <div class=\"pairValue\">AM32511</div>\r\n            \r\n            \r\n\t\t\t\t\t</div>\r\n            <div class=\"clear\"></div><br /><br />\r\n        </div>\r\n        <div class=\"floatRight grid7\">\r\n"
-
 -- Get HTML from trafikstyrelsen.
 getVIN :: String -> IO String
 getVIN a = do
