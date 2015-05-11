d5e6cc4870fe5537e3c113cded0ea36bfe9f8153
morten <morten@aeai.dk>
2015-05-11 20:54:35 +0200

    Parsec added.

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,7 +1,25 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Utils (firstMaybe, following, getFirstParameter) where
 
 import Data.List.Split
 import Data.List
+import qualified Text.Parsec as Parsec
+
+-- I am the error message infix operator, used later:
+import Text.Parsec ((<?>))
+
+-- Imported so we can play with applicative things later.
+-- not qualified as mostly infix operators we'll be using.
+import Control.Applicative
+
+-- Get the Identity monad from here:
+import Control.Monad.Identity (Identity)
+
+-- alias Parsec.parse for more concise usage.
+parse rule text = Parsec.parse rule "(source)" text
+
+h = "/tinglysning/forespoerg/bilbogen/bilbogen.xhtml;TDK_JSESSIONID=E4gvjvzPV_6nECcCdeE8POCn_QtrIGoS0LU6---cO4zQBWaWw0bX!-478567563!1440987210?_afPfm=-11ykkgvvlm&some=234"
+
 
 firstMaybe :: [a] -> Maybe a
 firstMaybe [] = Nothing
@@ -26,6 +44,12 @@ getFirstParameter a = do
   a1 <- getParametersAsString a
   a2 <- getFirstParameter' a1
   return a2
+
+myParser :: Parsec.Parsec String () String
+myParser = do
+  letters <- Parsec.many1 Parsec.letter
+  return $ letters
+
 
 
 
