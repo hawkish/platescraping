17db438ba2d8e1c935bc7c8999685784b2489c5d
morten <morten@aeai.dk>
2015-06-05 00:34:13 +0200

    Cookies continued. Still not working, but getting closer to find the bug.

--- src/Tinglysning.hs
+++ src/Tinglysning.hs
@@ -9,6 +9,7 @@ import Data.List
 import Data.List.Split
 import Data.Char
 import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.ByteString.Lazy as R
 import qualified Data.ByteString.Char8 as B
 import Utils (firstMaybe, following, getParameterAt, getCookie)
 import Control.Monad
@@ -17,6 +18,8 @@ import Control.Monad.List
 import Network
 import Data.Time.Clock
 import Data.Time.Calendar
+import qualified Control.Exception as E
+import Network.HTTP.Types.Status (statusCode)
 
 
 
@@ -78,7 +81,7 @@ getHTMLTinglysning = do
      let cookie = snd response
      return $ Just (html, cookie)
 
-postFormAtTinglysning :: IO (Maybe String)
+postFormAtTinglysning :: IO (Maybe (String, CookieJar))
 postFormAtTinglysning = do
   parameterAndCookie <- getParameterAndCookie
   case parameterAndCookie of
@@ -88,12 +91,16 @@ postFormAtTinglysning = do
      let cookie = snd parameterAndCookie
      let url = "http://www.tinglysning.dk/tinglysning/forespoerg/bilbogen/bilbogen.xhtml?" ++ _afPfm
      putStrLn url
-     html <- try $ doPostRequest url cookie "WAUZZZ8P2AA090943" :: IO (Either SomeException String)
-     case html of
+     response <- try $ doPostRequest' url cookie "WAUZZZ8P2AA090943" :: IO (Either SomeException (String, CookieJar))
+     case response of
       Left ex -> do
         putStrLn $ show ex
         return Nothing
-      Right html -> return $ Just html
+      Right response -> do
+        let html = fst response
+        let cookie = snd response
+        return $ Just (html, cookie)
+        
                       
 
 doGetRequest :: String -> IO (String, CookieJar)
@@ -109,6 +116,9 @@ doGetRequest url = do
 doPostRequest :: String -> CookieJar -> String -> IO String
 doPostRequest url cookie vin = do
   request' <- parseUrl url
+  let request = request' { method = "POST"
+                         , cookieJar = Just cookie }
+{--
   let request = urlEncodedBody [("soegemaade", "content:center:bilbogen:stelnrOption"),
                                 ("content:center:bilbogen:stelnr", B.pack(vin)),
                                 ("content:center:bilbogen:cvr", ""),
@@ -121,13 +131,60 @@ doPostRequest url cookie vin = do
                                 ("source","content:center:bilbogen:j_id150")] $ request' {
         secure = False
         , method = "POST"
-        , cookieJar = Just cookie 
+        , cookieJar = Just $ cookie
         , requestHeaders = [("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:37.0) Gecko/20100101 Firefox/37.0")]}
-  
+--}  
     --resp <- withManager $ httpLbs req
   --return $ L.unpack $ responseBody resp
   withManager $ \manager -> do
     res <- httpLbs request manager
     return $ L.unpack $ responseBody res
+
+
+past :: UTCTime
+past = UTCTime (ModifiedJulianDay 56200) (secondsToDiffTime 0)
+
+future :: UTCTime
+future = UTCTime (ModifiedJulianDay 562000) (secondsToDiffTime 0)
+
+cookie :: Cookie
+cookie = Cookie { cookie_name = "password_hash"
+                 , cookie_value = "abf472c35f8297fbcabf2911230001234fd2"
+                 , cookie_expiry_time = future
+                 , cookie_domain = "example.com"
+                 , cookie_path = "/"
+                 , cookie_creation_time = past
+                 , cookie_last_access_time = past
+                 , cookie_persistent = False
+                 , cookie_host_only = False
+                 , cookie_secure_only = False
+                 , cookie_http_only = False
+                 }
+
+--doPostRequest' :: String -> CookieJar -> String -> IO String
+
+doPostRequest' :: String -> CookieJar -> String -> IO (String, CookieJar)
+doPostRequest' url cookie vin = do
+  initReq <- parseUrl url
+  let req' = initReq {
+        secure = False
+        , method = "POST"
+        , cookieJar = Just cookie
+        --cookieJar = Just $ createCookieJar [cookie] 
+        , requestHeaders = [("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:37.0) Gecko/20100101 Firefox/37.0")]
+        }
+  let req = urlEncodedBody [("soegemaade", "content:center:bilbogen:stelnrOption"),
+                                ("content:center:bilbogen:stelnr", B.pack(vin)),
+                                ("content:center:bilbogen:cvr", ""),
+                                ("content:center:bilbogen:navn", ""),
+                                ("content:center:bilbogen:foedselsdato", ""),
+                                ("bogsattest", "content:center:bilbogen:uofficiel"),
+                                ("org.apache.myfaces.trinidad.faces.FORM", "j_id4"),
+                                ("_noJavaScript", "false"),
+                                ("javax.faces.ViewState","!-fkpwmjj67"),
+                                ("source","content:center:bilbogen:j_id150")] $ req'
   
+  resp <- withManager $ httpLbs req
+  return (L.unpack $ responseBody resp, responseCookieJar resp)
+ 
 
