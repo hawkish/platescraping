6954e55785d90069288c2f4438db6c0d43746ca7
hawkish <mortenhogh@gmail.com>
2015-08-19 18:58:33 +0200

    Added validation.

--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,9 @@
 import Trafikstyrelsen (getSurveyorRapports)
 import Tinglysning (getLandRegister)
 import ErrorType (initError, Error)
-import Data.String (fromString)
 
 
+import Data.Char
 import qualified Data.Text as T
 import qualified Data.Text.Lazy.Encoding as TLE
 import qualified Data.Text.Lazy as TL
@@ -38,8 +38,7 @@ main = scotty 3000 $ do
 
   get "/" $ do
     status status403
-    let error = TLE.decodeUtf8 $ encode $ initError (T.pack "403") (T.pack $ fromString $ "Forbidden.")
-    json error
+    json $ errorJSON "403" "Forbidden."
   
   get "/1.0/registrationnumber/:rn" $ do
     rn <- param "rn"
@@ -47,24 +46,29 @@ main = scotty 3000 $ do
     case result of
       Nothing -> do
         status status404
-        let error = TLE.decodeUtf8 $ encode $ initError (T.pack "404") (T.pack "Søgningen gav ikke noget resultat.")
-        json error
+        json $ errorJSON "404" "Søgningen gav intet resultat."
       Just result -> json result
 
   get "/1.0/vin/:vin" $ do
     vin <- param "vin"
-    result <- liftIO $ searchUsingVin vin
-    case result of
-      Nothing -> do
-        status status404
-        let error = TLE.decodeUtf8 $ encode $ initError (T.pack "404") (T.pack "Søgningen gav ikke noget resultat.")
-        json error
-      Just result -> json result
+    if validateVIN vin
+      then do 
+      result <- liftIO $ searchUsingVin vin
+      case result of
+        Nothing -> do
+          status status404
+          json $ errorJSON "404" "Søgningen gav intet resultat."
+        Just result -> json result 
+      else do
+      status status404
+      json $ errorJSON "404" "Ikke et gyldigt stel nummer."
+     
 
   notFound $ do
     status status404
-    let error = TLE.decodeUtf8 $ encode $ initError (T.pack "404") (T.pack $ fromString $ "Kan ikke finde servicen.")
-    json error
+    json $ errorJSON "404" "Kan ikke finde servicen."
+
+errorJSON a b = TLE.decodeUtf8 $ encode $ initError (T.pack a) (T.pack b)
 
 searchUsingReg :: String -> IO (Maybe TL.Text)
 searchUsingReg reg = do
@@ -79,3 +83,15 @@ searchUsingVin vin = do
   case landRegister of
     Nothing -> return Nothing
     Just landRegister -> return $ Just $ TLE.decodeUtf8 $ encode landRegister
+
+-- The rules in this wikipedia article is used.
+-- https://en.wikipedia.org/wiki/Vehicle_identification_number
+-- No VIN check digit calculation is performed.
+validateVIN :: String -> Bool
+validateVIN a = length a == 17 && (and $ map isDigitOrUpperLetter a)
+
+isDigitOrUpperLetter :: Char -> Bool
+isDigitOrUpperLetter a
+  | isDigit a = True 
+  | isLetter a && isUpper a && a /= 'Q' && a /= 'O' && a /= 'I' = True
+  | otherwise = False
